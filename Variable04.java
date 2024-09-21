c349ad1cc6ad6cac48132ad2132da059444a4c94
Ghefira Nur Fatimah <ghefiraaaaz9@gmail.com>
2024-09-21 16:18:47 +0800

    Add files via upload
    double_boolean

---
+++ Variable04.java
@@ -0,0 +1,23 @@
+public class Variable04 {
+    public static void main(String[] args) {
+        //deklarasi
+        double bil01, bil02;
+        boolean status;
+
+        //input
+        bil01 = 100.5;
+        bil02 = 10.5;
+
+        //proses
+        status = bil01 < bil02;
+
+        //output
+        System.out.println("____________________________________");
+        System.out.println("                    ");
+        System.out.println(bil01 + " < " + bil02);
+        System.out.println("Adalah = " + status);
+        System.out.println("                ");
+        System.out.println("____________________________________");
+
+    }
+}
